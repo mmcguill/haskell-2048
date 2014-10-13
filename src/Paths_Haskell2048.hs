910feba2beee8768ca2a79bea1ee66e3f8347389
Mark Mc <mmcguill@gmail.com>
2014-10-13 15:37:59 +0100

    Extra logging for heroku

--- src/Paths_Haskell2048.hs
+++ src/Paths_Haskell2048.hs
@@ -5,4 +5,6 @@ import System.FilePath
 -- This module will be replaced by cabal automatically and will be return the correct paths. THis is for development only
 
 getDataFileName :: FilePath -> IO FilePath
-getDataFileName a = return $ "../" ++ a -- Paths will be relative to root dir though we will be in src dir for development...
+getDataFileName a = do 
+	putStrLn "Yoyo!!"
+	return $ "../" ++ a -- Paths will be relative to root dir though we will be in src dir for development...
